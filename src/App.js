b1404fd75d63577d85805a58bfbe2defd12d3c1f
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-04-14 01:52:31 -0700

    can move piece with suggested positions

--- src/App.js
+++ src/App.js
@@ -18,7 +18,10 @@ class App extends React.Component {
         [0, 0, 0, 0, 0, 0, 0, 0],
         ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
         ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
-      ]
+      ],
+      targetPiece: null,
+      lastMovedPiece: null,
+
     }
     this.updateSize = this.updateSize.bind(this);
   }
@@ -42,21 +45,49 @@ class App extends React.Component {
   }
 
   handleClick(target) {
-    // if a piece is clicked
     console.log(target);
+    // find and highlight the potential moves
+    const currentPosition = target.dataset.pos;
+    const rowIndex = Number(currentPosition[0]);
+    const colIndex = Number(currentPosition[1]);
+
+    // if a piece is clicked
     if (target.className.includes('piece')) {
       // highlight the piece
       this.highlightPiece(target);
-      // find and highlight the potential moves
-      const currentPosition = target.dataset.pos;
-      const rowIndex = Number(currentPosition[0]);
-      const colIndex = Number(currentPosition[1]);
+      // undo cube highlight
+      this.setState(state => {
+        //undo highlight of cube
+        state.board.forEach((row, index) => {
+          let highlighted = row.indexOf(1);
+          if (highlighted !== -1) {
+            state.board[index][highlighted] = 0;
+          }
+        });
+        return state;
+      })
 
+      // record the target piece in state
+      let piece;
+      if (target.className.includes('red')) {
+        piece = 'r';
+      } else {
+        piece = 'b'
+      }
+      const targetPiece = [piece, rowIndex, colIndex];
+      this.setState({ targetPiece })
+      // find next two positions and highlight it
       this.findNextPosition(rowIndex, colIndex);
     }
 
     // if a potential spot is clicked
-
+    if (target.className.includes('highlight')) {
+      const curColor = this.state.targetPiece[0];
+      // check if this color is moved before
+      if (curColor !== this.state.lastMovedPiece) {
+        this.movePiece(rowIndex, colIndex);
+      }
+    }
   }
 
   highlightPiece(target) {
@@ -64,18 +95,26 @@ class App extends React.Component {
   }
 
   findNextPosition(row, col) {
-    const board = this.state.board.slice();
+    const board = this.state.board;
     // if it is a red piece
     if (board[row][col] === 'r') {
       if (row + 1 < board.length && // row in bound
           col + 1 < board.length && // col in bound
           board[row + 1][col + 1] === 0) { // it is an empty
-        board[row + 1][col + 1] = 1;
+        // board[row + 1][col + 1] = 1;
+        this.setState(state => {
+          state.board[row + 1][col + 1] = 1;
+          return state;
+        })
       }
       if (row + 1 < board.length &&
           col - 1 >= 0 &&
           board[row + 1][col - 1] === 0) {
-        board[row + 1][col - 1] = 1;
+        // board[row + 1][col - 1] = 1;
+        this.setState(state => {
+          state.board[row + 1][col - 1] = 1;
+          return state;
+        })
       }
     }
     // if it is a black piece
@@ -83,16 +122,45 @@ class App extends React.Component {
       if (row - 1 >= 0 && // row in bound
         col + 1 < board.length && // col in bound
         board[row - 1][col + 1] === 0) { // it is an empty
-        board[row - 1][col + 1] = 1;
+        // board[row - 1][col + 1] = 1;
+        this.setState(state => {
+          state.board[row - 1][col + 1] = 1;
+          return state;
+        })
       }
       if (row - 1 >= 0 &&
         col - 1 >= 0 &&
         board[row - 1][col - 1] === 0) {
-        board[row - 1][col - 1] = 1;
+        // board[row - 1][col - 1] = 1;
+        this.setState(state => {
+          state.board[row - 1][col - 1] = 1;
+          return state;
+        })
       }
     }
 
-    this.setState({ board });
+  }
+
+  movePiece(r, c) {
+    const curRow = this.state.targetPiece[1];
+    const curCol = this.state.targetPiece[2];
+
+    this.setState(state => {
+      // move the piece
+      state.board[curRow][curCol] = 0;
+      state.board[r][c] = state.targetPiece[0];
+
+      //undo highlight of cube
+      state.board.forEach((row, index) => {
+        let highlighted = row.indexOf(1);
+        if (highlighted !== -1) {
+          state.board[index][highlighted] = 0;
+        }
+      })
+      // record last moved piece
+      state.lastMovedPiece = state.targetPiece[0];
+      return state;
+    })
   }
 
 
@@ -108,16 +176,21 @@ class App extends React.Component {
             return (
               <div className="row" key={i}>
                 {row.map((cube, j) => {
+                  const pos = i.toString() + j.toString();
                   if (cube === 'r') return (
                     <div key={j} className="cube">
-                      <Piece color="red" position={i.toString() + j.toString()} />
+                      <Piece color="red" position={pos} />
                     </div>);
                   if (cube === 'b') return (
                     <div key={j} className="cube">
-                      <Piece color="black" position={i.toString() + j.toString()} />
+                      <Piece color="black" position={pos} />
                     </div>);
-                  if (cube === 0) return (<div key={j} className="cube"></div>);
-                  if (cube === 1) return (<div key={j} className="cube" style={highlightCubeStyle}></div>)
+                  if (cube === 0) return (
+                    <div key={j} className="cube empty" data-pos={pos}>
+                    </div>);
+                  if (cube === 1) return (
+                    <div key={j} className="cube highlight" data-pos={pos} style={highlightCubeStyle}>
+                  </div>)
                 })}
               </div>
             )
